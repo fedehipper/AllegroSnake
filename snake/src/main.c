d0ec4729ad3dd289e520b0b5f14d756549d87de2
fedehipper <federicohipper@gmail.com>
2016-07-20 19:22:04 -0300

    continuacion

--- snake/src/main.c
+++ snake/src/main.c
@@ -7,15 +7,12 @@
 
 int main(void) {
 	allegro_init();
-
 	install_keyboard();
-
 	set_color_depth(32);
-
 	request_refresh_rate(70);
 
-	BITMAP *bmp = create_bitmap(640, 480);
-	clear_bitmap(bmp);
+//	BITMAP *bmp = create_bitmap(640, 480);
+//	clear_bitmap(bmp);
 
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
 
@@ -25,14 +22,33 @@ int main(void) {
 	line(screen, 630, 470, 630, 10, palette_color[15]);
 	line(screen, 630, 470, 10, 470, palette_color[15]);
 
+
+
+	int a,w,s,d;
+	a = 10;
+	w = 10;
+	s = 10;
+	d = 10;
+
+
 	while(!key[KEY_ESC]) {
+
+
+		if(key[KEY_A]) rectfill(screen, a++, w, d, s, palette_color[15]);
+		if(key[KEY_W]) rectfill(screen, a, w++, d, s, palette_color[15]);
+		if(key[KEY_D]) rectfill(screen, a, w, d++, s, palette_color[15]);
+		if(key[KEY_S]) rectfill(screen, a, w, d, s++, palette_color[15]);
+
+		readkey();
+
 
 
 	}
 
-
 	remove_keyboard();
-	destroy_bitmap(bmp);
+	//destroy_bitmap(bmp);
+
 	allegro_exit();
 	return EXIT_SUCCESS;
 }
+END_OF_MAIN();
