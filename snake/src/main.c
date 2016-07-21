abdb0f9187061fdab7583414e669ab68a96870db
fedehipper <federicohipper@gmail.com>
2016-07-20 22:56:38 -0300

    refactor

--- snake/src/main.c
+++ snake/src/main.c
@@ -33,12 +33,6 @@ int main(void) {
 	set_color_depth(32);
 	request_refresh_rate(70);
 
-
-
-
-//	BITMAP *bmp = create_bitmap(640, 480);
-//	clear_bitmap(bmp);
-
 	if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0) != 0) {
 		set_gfx_mode(GFX_AUTODETECT_WINDOWED, 0, 0, 0, 0);
 		allegro_message("Incapaz de entrar a modo grafico\n%s\n", allegro_error);
@@ -59,60 +53,18 @@ int main(void) {
 			tecla = readkey() >> 8;
 			switch(tecla) {
 			case TEC_ARRIBA:
-				incX = 0;
-				incY = -1;
-				break;
+				incX = 0; incY = -1; break;
 			case TEC_ABAJO:
-				incX = 0;
-				incY = 1;
-				break;
+				incX = 0; incY = 1; break;
 			case TEC_IZQDA:
-				incX = -1;
-				incY = 0;
-				break;
+				incX = -1; incY = 0; break;
 			case TEC_DCHA:
-				incX = 1;
-				incY = 0;
-				break;
+				incX = 1; incY = 0; break;
 			}
 		}
 		rest(PAUSA);
-
 	}
-	while(TRUE);
-
-
-	readkey();
-
-//	int a,w,s,d;
-//	a = 10;
-//	w = 10;
-//	s = 10;
-//	d = 10;
-//
-//
-//	while(!key[KEY_ESC]) {
-//
-//
-//		if(key[KEY_A]) rectfill(screen, a++, w, d, s, palette_color[15]);
-//		if(key[KEY_W]) rectfill(screen, a, w++, d, s, palette_color[15]);
-//		if(key[KEY_D]) rectfill(screen, a, w, d++, s, palette_color[15]);
-//		if(key[KEY_S]) rectfill(screen, a, w, d, s++, palette_color[15]);
-//
-//		readkey();
-//
-//
-//
-//	}
-//	do
-//	{
-//	  /*Actualizar reloj y pantalla, mover nave, etc */
-//	}
-//	while  ( ! keypressed () );
-//	teclaPulsada = readkey();
-//
-//	remove_keyboard();
-	//destroy_bitmap(bmp);
+	while(!key[KEY_ESC]);
 
 	allegro_exit();
 	return EXIT_SUCCESS;
