8a511dc96e6605cc6f8064ae022c0bb57ac1606f
fedehipper <federicohipper@gmail.com>
2016-07-21 22:40:52 -0300

    refactor

--- snake/src/main.c
+++ snake/src/main.c
@@ -5,39 +5,29 @@
 
 #define POS_X_INI 32
 #define POS_Y_INI 20
-
 #define INC_X_INI 1
 #define INC_Y_INI 0
 
 #define PAUSA 100
-
-#define ARRIBA KEY_W
-#define ABAJO KEY_S
-#define IZQUIERDA KEY_A
-#define DERECHA KEY_D
-
 int tecla;
 
 #define ESCALA 10
 
-#define ANCHOTROZO 10
-#define ALTOTROZO 10
+#define ANCHO_TROZO 10
+#define ALTO_TROZO 10
 
-#define MAXFILAS 40
-#define MAXCOLS 64
+#define MAX_FILAS 40
+#define MAX_COLS 64
 
-int mapa[MAXFILAS][MAXCOLS] = {{0}};
+int mapa[MAX_FILAS][MAX_COLS] = {{0}};
 
 bool toca_pared(int pos_x, int pos_y) {
 	return pos_x >= 63 || pos_x < 1 || pos_y >= 39 || pos_y < 1;
 }
 
-
-int numFrutas = 7;
-
 BITMAP *ladrilloFondo, *comida, *jugador;
 
-typedef int tipoSnake[ANCHOTROZO][ALTOTROZO];
+typedef int tipoSnake[ANCHO_TROZO][ALTO_TROZO];
 
 tipoSnake vivora =
    {{0,15,15,15,15,15,15,15,0},
@@ -50,44 +40,43 @@ tipoSnake vivora =
     {15,15,0,0,0,0,0,15,15},
     {0,15,15,15,15,15,15,15,0}};
 
-void crearSnake() {
-  int i, j;
-  jugador = create_bitmap(10, 10);
-  clear_bitmap(jugador);
-  for(i = 0; i < ANCHOTROZO; i++)
-    for (j = 0; j < ALTOTROZO; j++)
-      putpixel(jugador, i, j, palette_color[vivora[j][i]]);
+void crear_snake() {
+	int i, j;
+	jugador = create_bitmap(10, 10);
+	clear_bitmap(jugador);
+	for(i = 0; i < ANCHO_TROZO; i++)
+		for (j = 0; j < ALTO_TROZO; j++)
+			putpixel(jugador, i, j, palette_color[vivora[j][i]]);
 }
 
-
-void dibujarMapa() {
+void dibujar_mapa() {
 	clear_bitmap(screen);
 	rect(screen, 10, 10, 630, 390, palette_color[15]);
 }
 
 int main(void) {
-	int posX, posY;
-	int incX, incY;
+	int pos_x, pos_y;
+	int inc_x, inc_y;
 	allegro_init();
 	install_keyboard();
 	install_timer();
 
 	set_gfx_mode(GFX_SAFE, 640, 400, 0, 0);
-	crearSnake();
+	crear_snake();
 
-	dibujarMapa();
+	dibujar_mapa();
 
-	posX = POS_X_INI;
-	posY = POS_Y_INI;
+	pos_x = POS_X_INI;
+	pos_y = POS_Y_INI;
 
-	incX = INC_X_INI;
-	incY = INC_Y_INI;
+	inc_x = INC_X_INI;
+	inc_y = INC_Y_INI;
 
-	  do {
-		dibujarMapa();
-		draw_sprite(screen, jugador, posX * ESCALA, posY * ESCALA);
+	do {
+		dibujar_mapa();
+		draw_sprite(screen, jugador, pos_x * ESCALA, pos_y * ESCALA);
 
-		if (toca_pared(posX, posY)) {
+		if (toca_pared(pos_x, pos_y)) {
 			allegro_message("Game Over");
 			break;
 		}
@@ -95,19 +84,19 @@ int main(void) {
 		if (keypressed()) {
 			tecla = readkey() >> 8;
 			switch (tecla) {
-			  case ARRIBA:
-				  incX = 0; incY = -1;  break;
-			  case ABAJO:
-				  incX = 0; incY = 1;  break;
-			  case IZQUIERDA:
-				  incX = -1; incY = 0;  break;
-			  case DERECHA:
-				  incX = 1; incY = 0;  break;
+			  case KEY_W:
+				  inc_x = 0; inc_y = -1;  break;
+			  case KEY_S:
+				  inc_x = 0; inc_y = 1;  break;
+			  case KEY_A:
+				  inc_x = -1; inc_y = 0;  break;
+			  case KEY_D:
+				  inc_x = 1; inc_y = 0;  break;
 			}
 		}
 
-		posX += incX;
-		posY += incY;
+		pos_x += inc_x;
+		pos_y += inc_y;
 
 		rest (PAUSA);
 	  }
@@ -116,5 +105,4 @@ int main(void) {
 	  readkey();
 	  return EXIT_SUCCESS;
 }
-
 END_OF_MAIN();
