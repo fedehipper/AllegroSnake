68e8aa1c4c299ed5a61a51b92906ae887371da03
fedehipper <federicohipper@gmail.com>
2016-09-30 14:22:40 -0300

    refactor

--- snake/src/funciones_snake.h
+++ snake/src/funciones_snake.h
@@ -5,12 +5,6 @@
 #define ANCHO 64
 
 void inicio(int *tam, char campo[ALTO][ANCHO]);
-void intro_datos(char campo[ALTO][ANCHO], int tam);
-void intro_campo(char campo[ALTO][ANCHO]);
-void draw(char campo[ALTO][ANCHO]);
 void loop(char campo[ALTO][ANCHO], int tam);
-int input(char campo[ALTO][ANCHO], int tam, int *muerto);
-void update(char campo[ALTO][ANCHO], int tam);
-void intro_datos_nuevos(char campo[ALTO][ANCHO], int tam);
 
 #endif
