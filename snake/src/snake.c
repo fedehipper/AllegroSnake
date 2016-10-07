ad740138055d7e3ea5b08b0c857883eac5d2018a
fedehipper <federicohipper@gmail.com>
2016-10-07 03:49:13 -0300

    refactor

--- snake/src/snake.c
+++ snake/src/snake.c
@@ -1,29 +1,17 @@
 #include <allegro.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
-
 
 #define ALTO 40
 #define ANCHO 64
 #define LIMITE_SNAKE 2560
 #define ESCALA 10
+#define TAMANIO_INICIAL 4
 #define TOTAL_FRUTAS 2556
-
-typedef struct {
-	int x,y;
-	int mod_x, mod_y;
-	char imagen;
-} snk;
-
-typedef struct {
-	int x,y;
-} frt;
-
-snk snake[LIMITE_SNAKE];
-
-frt fruta;
+#define MAX_DIGITOS_PUNTAJE 4
 
 typedef int tipo_snake[10][10];
 
@@ -48,7 +36,19 @@ tipo_snake fruta_bits =
 	{0,0,0,15,15,15,0,0,0},
     {0,0,0,15,15,15,0,0,0},
     {0,0,0,15,15,15,0,0,0}};
+typedef struct {
+	int x,y;
+	int mod_x, mod_y;
+	char imagen;
+}snk;
 
+typedef struct {
+	int x,y;
+}frt;
+
+snk snake[LIMITE_SNAKE];
+
+frt fruta;
 
 BITMAP *comida, *jugador, *cabeza;
 
@@ -81,9 +81,8 @@ void crear_snake(void) {
 	jugador = create_bitmap(ESCALA, ESCALA);
 	clear_bitmap(jugador);
 	for(i = 0 ; i < ESCALA ; i++) {
-		for (j = 0 ; j < ESCALA ; j++) {
+		for (j = 0 ; j < ESCALA ; j++)
 			putpixel(jugador, i, j, palette_color[vivora[j][i]]);
-		}
 	}
 }
 
@@ -92,9 +91,8 @@ void crear_comida(void) {
 	comida = create_bitmap(ESCALA, ESCALA);
 	clear_bitmap(comida);
 	for(i = 0 ; i < ESCALA ; i++) {
-		for (j = 0 ; j < ESCALA ; j++) {
+		for (j = 0 ; j < ESCALA ; j++)
 			putpixel(comida, i, j, palette_color[fruta_bits[j][i]]);
-		}
 	}
 }
 
@@ -121,9 +119,8 @@ void draw(char campo[ALTO][ANCHO], int puntaje_record, int puntaje_actual) {
 void intro_campo(char campo[ALTO][ANCHO]) {
 	int i, j;
 	for(i = 0; i < ALTO; i++) {
-		for(j = 0; j < ANCHO; j++) {
+		for(j = 0; j < ANCHO; j++)
 			campo[i][j] = ' ';
-		}
 	}
 }
 
@@ -142,18 +139,59 @@ void intro_datos(char campo[ALTO][ANCHO], int tam) {
 	campo[fruta.y][fruta.x] = '%';
 }
 
-void inicio(int *tam, char campo[ALTO][ANCHO]) {
+void seleccionar_nivel(char campo[ALTO][ANCHO], int *nivel) {
+	while(1) {
+		dibujar_bordes();
+		textprintf_justify_ex(screen, font, 30, 10, 20, 0, 15, 0, "SELECT LEVEL: ");
+		textprintf_justify_ex(screen, font, 30, 10, 40, 0, 15, 0, "1 EASY");
+		textprintf_justify_ex(screen, font, 30, 10, 60, 0, 15, 0, "2 MEDIUM");
+		textprintf_justify_ex(screen, font, 30, 10, 80, 0, 15, 0, "3 PROFFESIONAL");
+		int tecla = 0;
+		if(keypressed()) {
+			tecla = readkey() >> 8;
+			switch (tecla) {
+				case KEY_1: case KEY_1_PAD: *nivel = 1;
+				break;
+				case KEY_2: case KEY_2_PAD: *nivel = 2;
+				break;
+				case KEY_3: case KEY_3_PAD: *nivel = 3;
+				break;
+				default:
+					textprintf_centre_ex(screen, font, 325, 190, 15, 0, "AGAIN SELECT A VALID LEVEL ...");
+					continue;
+				break;
+			}
+			break;
+		}
+	}
+}
+
+void leer_puntaje_record(int * nivel, FILE * archivo, int *puntaje_record) {
+	if(archivo != NULL) {
+		fscanf(archivo, "%d", puntaje_record);
+		fclose(archivo);
+	}
+	else
+		*puntaje_record = 0;
+
+}
+
+void inicio(int *tam, char campo[ALTO][ANCHO], int *nivel, int * puntaje_record, FILE * archivo) {
 	allegro_init();
 	install_keyboard();
 	install_timer();
 	set_gfx_mode(GFX_SAFE, ANCHO * 10, ALTO * 10, 0, 0);
+
+	seleccionar_nivel(campo, nivel);
+	leer_puntaje_record(nivel, archivo, puntaje_record);
+
 	crear_snake();
 	crear_comida();
 
 	snake[0].x = 32;
 	snake[0].y = 10;
 
-	*tam = 4;
+	*tam = TAMANIO_INICIAL;
 	srand(time(NULL));
 
 	fruta.x = rand() % (ANCHO - 2) + 1;
@@ -178,17 +216,14 @@ void intro_datos_nuevos(char campo[ALTO][ANCHO], int tam) {
 	snake[0].x += snake[0].mod_x;
 	snake[0].y += snake[0].mod_y;
 
-	for(i = 0 ; i < tam ; i++) {
+	for(i = 0 ; i < tam ; i++)
 		campo[snake[i].y][snake[i].x] = snake[i].imagen;
-	}
 	campo[fruta.y][fruta.x] = '%';
 }
 
 void update(char campo[ALTO][ANCHO], int tam, int muerto) {
 	intro_campo(campo);
-	if(muerto == 0) {
-		intro_datos_nuevos(campo, tam);
-	}
+	if(muerto == 0) intro_datos_nuevos(campo, tam);
 }
 
 void asignar_movimiento(int mov_x, int mov_y) {
@@ -208,18 +243,13 @@ void exit_snake(void) {
 int input(char campo[ALTO][ANCHO], int tam, int *muerto, int puntaje_record) {
 	int tecla = 0;
 	if(*muerto == 0) {
-		if(snake[0].x == 0 || snake[0].x == ANCHO - 1 || snake[0].y == 0 || snake[0].y == ALTO - 1) {
-			*muerto = 1;
-		}
+		if(snake[0].x == 0 || snake[0].x == ANCHO - 1 || snake[0].y == 0 || snake[0].y == ALTO - 1) *muerto = 1;
 	}
 
 	if(*muerto == 0) {
 		int i;
-		for(i = 1 ; i < tam && *muerto == 0 ; i++) {
-			if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
-				*muerto = 1;
-			}
-		}
+		for(i = 1 ; i < tam && *muerto == 0 ; i++)
+			if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) *muerto = 1;
 	}
 
 	if(*muerto == 0) {
@@ -243,76 +273,70 @@ int input(char campo[ALTO][ANCHO], int tam, int *muerto, int puntaje_record) {
 				readkey();
 			}
 
-			if(tecla == KEY_DOWN && snake[0].mod_y != -1) {
-				asignar_movimiento(0, 1);
-			}
-			if(tecla == KEY_UP && snake[0].mod_y != 1) {
-				asignar_movimiento(0, -1);
-			}
-			if(tecla == KEY_LEFT && snake[0].mod_x != 1) {
-				asignar_movimiento(-1, 0);
-			}
-			if(tecla == KEY_RIGHT && snake[0].mod_x != -1) {
-				asignar_movimiento(1, 0);
-			}
-		}
-	} else {
-		if(tam - 4 == TOTAL_FRUTAS) {
-			textprintf_centre_ex(screen, font, 325, 190, 15, 0, "YOU WIN!");
+			int mov_x = 0, mov_y = 0;
 
+			if(tecla == KEY_DOWN && snake[0].mod_y != -1) mov_y = 1;
+			if(tecla == KEY_UP && snake[0].mod_y != 1) mov_y = -1;
+			if(tecla == KEY_LEFT && snake[0].mod_x != 1) mov_x = -1;
+			if(tecla == KEY_RIGHT && snake[0].mod_x != -1) mov_x = 1;
+			if(mov_x != 0 || mov_y != 0)
+				asignar_movimiento(mov_x, mov_y);
+		}
+	}
+	else {
+		if(tam - TAMANIO_INICIAL == TOTAL_FRUTAS) {
+			textprintf_centre_ex(screen, font, 325, 190, 15, 0, "YOU WIN!");
 		} else {
 			textprintf_centre_ex(screen, font, 325, 190, 15, 0, "GAME OVER");
-			textprintf_centre_ex(screen, font, 325, 210, 15, 0, "SCORE: %d", tam - 4);
+			textprintf_centre_ex(screen, font, 325, 210, 15, 0, "SCORE: %d", tam - TAMANIO_INICIAL);
 			textprintf_centre_ex(screen, font, 325, 220, 15, 0, "RECORD: %d", puntaje_record);
 		}
 		exit_snake();
-		}
+	}
 	return tam;
 }
 
-void loop(char campo[ALTO][ANCHO], int tam, int puntaje_record, FILE * archivo) {
-	int muerto = 0, pausa = 250, veces = 0;
+void loop(char campo[ALTO][ANCHO], int tam, int puntaje_record, FILE * archivo, int *nivel) {
+	int muerto = 0, pausa = 0;
+
+	switch(*nivel) {
+		case 1 :
+			pausa = 300;
+			break;
+		case 2 :
+			pausa = 200;
+			break;
+		case 3 :
+			pausa = 100;
+			break;
+	}
 
 	do {
-		draw(campo, puntaje_record, tam - 4);
+		draw(campo, puntaje_record, tam - TAMANIO_INICIAL);
 		tam = input(campo, tam, &muerto, puntaje_record);
-
 		update(campo, tam, muerto);
-
-		if(veces > 10) {
-			veces = 0;
-			pausa -= 1;
-		}
 		rest(pausa);
 	} while (muerto == 0);
 
 	int puntaje_actual = tam - 4;
+
 	archivo = fopen("puntaje_record.txt", "w");
-	if(puntaje_record <= puntaje_actual)
-		fprintf(archivo, "%d", puntaje_actual);
-	else
-		fprintf(archivo, "%d", puntaje_record);
+	(puntaje_record <= puntaje_actual) ? fprintf(archivo, "%d", puntaje_actual) : fprintf(archivo, "%d", puntaje_record);
+
 	fclose(archivo);
 }
-
 
 int main(void) {
 	int tam;
 	char campo[40][64];
+	int puntaje_record;
 
 	FILE * archivo;
-	int puntaje_record;
 	archivo = fopen("puntaje_record.txt", "r");
 
-	if(archivo != NULL) {
-		fscanf(archivo, "%d", &puntaje_record);
-		fclose(archivo);
-	}
-	else
-		puntaje_record = 0;
-
-	inicio(&tam, campo);
-	loop(campo, tam, puntaje_record, archivo);
+	int nivel = 0;
+	inicio(&tam, campo, &nivel, &puntaje_record, archivo);
+	loop(campo, tam, puntaje_record, archivo, &nivel);
 
 	allegro_exit();
 	return EXIT_SUCCESS;
